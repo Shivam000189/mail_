6cc516c185656740769085530c51108ae11462ee
Shivam000189 <shivamsharmass9897@gmail.com>
2026-01-14 22:40:48 +0530

    frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,23 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [topic, setTopic] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleGenerate = () => {
+    setEmail(`This is a professional email about ${topic}`);
+  }
 
   return (
     <>
-      
+      <h1>AI Email Generator</h1>
+      <input type="text" value={topic} onChange={(e) => setTopic(e.target.value)}/>
+      <button onClick={handleGenerate}>Generate</button>
+
+      <div className='h-100 w-100 box-border size-32 border-4 p-4 mt-10'>
+           
+          {email}
+      </div>
     </>
   )
 }

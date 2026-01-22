3f7e629b3f7db9a3e2e54667f729772c1286533d
Shivam000189 <shivamsharmass9897@gmail.com>
2026-01-22 20:12:38 +0530

    co

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+import path from 'path';
 
 const router = require('./routes/emailRoutes')
 
 const app = express();
+app.use(express.static("dist"));
 
 app.use(cors());
 app.use(express.json());
@@ -13,9 +15,9 @@ app.use(express.json());
 app.use('/api', router);
 
 
-app.get("/", (req, res) => {
-    res.send('Backend Working');
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

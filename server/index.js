606c2b0ccf22dd383966905b700e9cbe642ba556
Shivam000189 <shivamsharmass9897@gmail.com>
2026-01-22 19:30:15 +0530

    Backend

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
     res.send('Backend Working');
 })
 
-
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     console.log('Server is running ')
 })
